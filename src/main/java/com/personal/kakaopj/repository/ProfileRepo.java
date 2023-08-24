da818a75f986f18d03d507ead57c8888ac4fe7cf
Hyo Gyeong <cdkrcd8@gmail.com>
2023-08-25 04:26:36 +0900

    add query

--- src/main/java/com/personal/kakaopj/repository/ProfileRepo.java
+++ src/main/java/com/personal/kakaopj/repository/ProfileRepo.java
@@ -2,9 +2,11 @@ package com.personal.kakaopj.repository;
 
 import com.personal.kakaopj.domain.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -12,6 +14,13 @@ import java.util.ArrayList;
 public interface ProfileRepo extends JpaRepository<Profile, Long> {
     ArrayList<Profile> findProfileByUserId(long userId);
 
+    Profile findProfileById(long profileId);
+
     @Query(value = "select * from Profile where user_id = :userId and is_multi_profile = 0", nativeQuery = true)
-    Profile getMainProfile(@Param("userId") long userId);
+    Profile getMyMainProfile(@Param("userId") long userId);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update Profile set name = :name, status_message = :msg where profile_id = :pfId", nativeQuery = true)
+    void updateUserProfile(@Param("name") String name, @Param("msg") String message, @Param("pfId") long profileId);
 }
