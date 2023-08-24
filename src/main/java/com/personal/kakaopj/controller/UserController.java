1171a5235d285b5829cf8180b6a16d1049d52713
Hyo Gyeong <cdkrcd8@gmail.com>
2023-08-25 04:27:26 +0900

    clean code

--- src/main/java/com/personal/kakaopj/controller/UserController.java
+++ src/main/java/com/personal/kakaopj/controller/UserController.java
@@ -4,10 +4,7 @@ import com.personal.kakaopj.dto.UserDetailProfileDto;
 import com.personal.kakaopj.dto.UserProfileDto;
 import com.personal.kakaopj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
