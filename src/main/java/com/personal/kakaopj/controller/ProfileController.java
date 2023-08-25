efbeff78cbf9151ff556d1da889e6b1f0c1305ce
Hyo Gyeong <cdkrcd8@gmail.com>
2023-08-25 15:55:48 +0900

    add profile edit & profile bg-img add controller

---
+++ src/main/java/com/personal/kakaopj/controller/ProfileController.java
@@ -0,0 +1,45 @@
+package com.personal.kakaopj.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.personal.kakaopj.dto.ProfileBgImgDto;
+import com.personal.kakaopj.dto.ProfileDto;
+import com.personal.kakaopj.service.ProfileBgImgService;
+import com.personal.kakaopj.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/")
+public class ProfileController {
+    @Autowired
+    private ProfileService profileService;
+    public void setProfileService(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @Autowired
+    private ProfileBgImgService profileBgImgService;
+
+    public void setProfileBgImgService(ProfileBgImgService profileBgImgService) {
+        this.profileBgImgService = profileBgImgService;
+    }
+
+    // 내 프로필 이름, 상태 메시지 수정
+    @PatchMapping("my-profile/edit/{profileId}")
+    public ProfileDto myProfileEdit(@PathVariable long profileId, @RequestParam String name,
+                                    @RequestParam String message){
+        profileService.editMyProfile(profileId, name, message);
+        return profileService.findProfileById(profileId);
+    }
+
+    // 내 프로필 배경 이미지 수정 - 추가
+    @PostMapping("my-profile/bg-img/edit/{profileId}")
+    public ArrayList<ProfileBgImgDto> myProfileBgImgAdd(@PathVariable long profileId, @RequestParam String bgimg) throws ParseException {
+        return profileBgImgService.addProfileBgImg(profileId, bgimg);
+    }
+
+    // 내 프로필 배경 이미지 수정 - 삭제
+}
