d488825ea91ce814ca1b9f36115af149052cd030
Hyo Gyeong <cdkrcd8@gmail.com>
2023-09-01 12:46:59 +0900

    move code to ProfileController

--- src/main/java/com/personal/kakaopj/controller/UserController.java
+++ src/main/java/com/personal/kakaopj/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.personal.kakaopj.controller;
 
-import com.personal.kakaopj.dto.UserDetailProfileDto;
-import com.personal.kakaopj.dto.UserDto;
-import com.personal.kakaopj.dto.UserProfileDto;
+import com.personal.kakaopj.dto.*;
 import com.personal.kakaopj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -15,18 +15,6 @@ public class UserController {
     private UserService userService;
     public void setUserService(UserService userService){
         this.userService = userService;
-    }
-
-    // index 화면 내 프로필 정보
-    @GetMapping("me/{userId}")
-    public UserProfileDto myProfile(@PathVariable long userId){
-        return userService.getMyProfile(userId);
-    }
-
-    // 내 프로필 상세 정보 : 이름, 상태 메시지, 프로필 사진 목록, 프로필 배경 사진 목록, 플레이리스트 및 음악목록
-    @GetMapping("me/detail/{userId}")
-    public UserDetailProfileDto myDetailProfile(@PathVariable long userId){
-        return userService.getMyDetailProfile(userId);
     }
 
     // 내 설정 값 조회 : 카카오 ID, email, 전화번호 etc
