89acbb554f9057f07aace7a7698568ea09dfdb14
Hyo Gyeong <cdkrcd8@gmail.com>
2023-11-02 14:09:51 +0900

    chore: add getter, setter annotation

--- src/main/java/com/personal/kakaopj/groupchat/domain/GroupChatRoom.java
+++ src/main/java/com/personal/kakaopj/groupchat/domain/GroupChatRoom.java
@@ -2,7 +2,11 @@ package com.personal.kakaopj.groupchat.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "GroupChatRoom")
 public class GroupChatRoom {
@@ -16,28 +20,4 @@ public class GroupChatRoom {
 
     @Column(name="update_date_time")
     private LocalDateTime updateDateTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreateDateTime() {
-        return createDateTime;
-    }
-
-    public void setCreateDateTime(LocalDateTime createDateTime) {
-        this.createDateTime = createDateTime;
-    }
-
-    public LocalDateTime getUpdateDateTime() {
-        return updateDateTime;
-    }
-
-    public void setUpdateDateTime(LocalDateTime updateDateTime) {
-        this.updateDateTime = updateDateTime;
-    }
 }
