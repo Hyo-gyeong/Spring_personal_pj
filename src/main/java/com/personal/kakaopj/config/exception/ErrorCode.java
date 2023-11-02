29f7f247267fd23ca6000249b78963794e70a09b
Hyo Gyeong <cdkrcd8@gmail.com>
2023-11-02 14:09:19 +0900

    fix: change type private to final

--- src/main/java/com/personal/kakaopj/config/exception/ErrorCode.java
+++ src/main/java/com/personal/kakaopj/config/exception/ErrorCode.java
@@ -20,9 +20,14 @@ public enum ErrorCode { // BaseResponseStatus와 같은 역할
     INVALID_ARGUMENTS(400, "invalid arguments" ,BAD_REQUEST),
     SERVER_ERROR(500, "server error", INTERNAL_SERVER_ERROR);
 
-    private int code;
-    private String message;
-    private HttpStatus httpStatus;
+    /*
+    Enum은 본질적으로 불변의 성격을 가지기 때문에 모든 멤버 변수는 final로 정의해서 사용해야 함.
+    왜냐하면 Enum타입은 인스턴스를 생성할 수 없고, 모든 열거 인자들이 public static final로 정의 되기 때문에
+    thread간 공유가 가능하여 멤버 변수를 final로 설정하지 않으면 의도하지 않게 변경된 값을 사용할 가능성이 존재하기 때문.
+     */
+    final int code;
+    final String message;
+    final HttpStatus httpStatus;
 
     ErrorCode(int code, String message, HttpStatus httpStatus) {
         this.code = code;
